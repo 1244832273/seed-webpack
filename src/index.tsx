36186c7df0e4f2bc14a5a2963072e774cb12cf98
lutianwen <tom_lu@yunquna.com>
2022-06-08 16:31:24 +0800

    初始化dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-/*
- * @Author: 鲁田文
- * @Date: 2022-03-02 11:52:31
- * @LastEditTime: 2022-06-02 18:28:35
- * @LastEditors: 鲁田文
- * @Description:
- */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ConfigProvider } from "antd";
+import locale from "antd/es/locale/zh_CN";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
 
-import Test1 from 'Pages/test1';
+ReactDOM.render(
+  <ConfigProvider locale={locale}>
+    <App />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<Test1 />, document.getElementById('root'));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
